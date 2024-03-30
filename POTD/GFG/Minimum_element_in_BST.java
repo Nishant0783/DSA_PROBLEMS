39fbb27bf4f4c681893e4650f08d62fea92dc9b6
Nishant Paliwal <141571596+Nishant0783@users.noreply.github.com>
2024-03-30 16:13:05 +0000

    POTD_30 March 2024

--- POTD/GFG/Minimum_element_in_BST.java
+++ POTD/GFG/Minimum_element_in_BST.java
@@ -1,4 +1,4 @@
-// Given the root of a Binary Search Tree. The task is to find the minimum valued element in this given BST
+// Given the root of a Binary Search Tree. The task is to find the minimum valued element in this given BST.
 
 // Date: 30 March 2024
 
