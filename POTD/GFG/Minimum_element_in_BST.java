c1127a19c8eb34abccf07ea43d3f39ed539f3a9b
Nishant Paliwal <141571596+Nishant0783@users.noreply.github.com>
2024-03-30 16:10:29 +0000

    POTD_30 March 2024

---
+++ POTD/GFG/Minimum_element_in_BST.java
@@ -0,0 +1,22 @@
+// Given the root of a Binary Search Tree. The task is to find the minimum valued element in this given BST
+
+// Date: 30 March 2024
+
+
+class Solution {
+    // Function to find the minimum element in the given BST.
+    int minValue(Node root) {
+        // code here
+         if (root == null) {
+            return -1;
+        }
+        
+      
+        while (root.left != null) {
+            root = root.left;
+        }
+        
+
+        return root.data;
+    }
+}
---
+++ POTD/LEETCODE/Subaarays_with_K_different_integers.java
@@ -0,0 +1,37 @@
+// Given an integer array nums and an integer k, return the number of good subarrays of nums.
+
+// A good array is an array where the number of different integers in that array is exactly k.
+
+// For example, [1,2,3,1,2] has 3 different integers: 1, 2, and 3.
+// A subarray is a contiguous part of an array.
+
+// Date: 30 March 2024
+
+class Solution {
+       public int subarraysWithKDistinct(int[] nums, int k) {
+        return atMostKDistinct(nums, k) - atMostKDistinct(nums, k - 1);
+    }
+    
+    private int atMostKDistinct(int[] nums, int k) {
+        int n = nums.length;
+        HashMap<Integer, Integer> frequencyMap = new HashMap<>();
+        int left = 0;
+        int right = 0;
+        int count = 0;
+        
+        while (right < n) {
+            frequencyMap.put(nums[right], frequencyMap.getOrDefault(nums[right], 0) + 1);
+            while (frequencyMap.size() > k) {
+                frequencyMap.put(nums[left], frequencyMap.get(nums[left]) - 1);
+                if (frequencyMap.get(nums[left]) == 0) {
+                    frequencyMap.remove(nums[left]);
+                }
+                left++;
+            }
+            count += right - left + 1;
+            right++;
+        }
+        
+        return count;
+    }
+}
